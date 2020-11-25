4e3b8356bcb7de9edff4cd48b62f826b1eb929a3
Nick Swoboda <nickswoboda@gmail.com>
2020-11-25 14:53:46 -0800

    Add income table and net worth

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
-const { ipcRenderer } = require('electron')
+const { ipcRenderer, remote } = require('electron')
 const fs = require('fs')
+const { parse } = require('path')
 
-function updateCategoryTotal(category, total, percentage)
+var expense_total = 0
+var income_total = 0
+
+function updateTotalPercentages(is_expense)
 {
-    let table = document.getElementById("totals-table")
+    let table = is_expense ? document.getElementById("expense-table") : document.getElementById("income-table")
 
+    for (i = 1; i < table.rows.length; ++i){
+        let amount = parseFloat(table.rows[i].cells[1].innerHTML)
+        let total = is_expense ? expense_total : income_total
+        let percentage = amount / total * 100
+        table.rows[i].cells[2].innerHTML = percentage
+    }
+
+    let net_income = income_total + expense_total
+    
+    let net_income_text = document.getElementById("net-income")
+    net_income_text.innerHTML = net_income
+
+    net_income_text.style.color = net_income < 0 ? "red" : "green" 
+
+
+}
+function updateCategoryTotal(category, amount, is_expense)
+{
+    let table = is_expense ? document.getElementById("expense-table") : document.getElementById("income-table")
+
+    if (is_expense){
+        expense_total += parseFloat(amount)
+    }else{
+        income_total += parseFloat(amount)
+    }
     for (i = 0; i < table.rows.length; ++i){
         row_category = table.rows[i].cells[0].innerHTML
         if (row_category === category){
-            table.rows[i].cells[1].innerHTML = total
-            table.rows[i].cells[2].innerHTML = percentage
+            let amount_cell = table.rows[i].cells[1]
+            let new_amount = parseFloat(amount_cell.innerHTML) + parseFloat(amount)
+            amount_cell.innerHTML = new_amount
+            updateTotalPercentages(is_expense)
             return
         }
     }
@@ -17,45 +48,12 @@ function updateCategoryTotal(category, total, percentage)
     var new_row = table.insertRow(1)
     let category_cell = new_row.insertCell(0)
     let amount_cell = new_row.insertCell(1)
-    let percent_cell = new_row.insertCell(2)
 
     category_cell.innerHTML = category 
-    amount_cell.innerHTML = total 
-    percent_cell.innerHTML = percentage
+    amount_cell.innerHTML = amount 
 
-}
-function updateTotalsTable()
-{
-    let categories = {} 
-    let total = 0
-
-    let table = document.getElementById("table")
-    var rows = table.querySelectorAll("tr")
-
-    for (var i = 1; i < rows.length; ++i){
-        var row = []
-        var cols = rows[i].querySelectorAll("td")
-
-        for (var j = 1; j < cols.length; ++j){
-            row.push(cols[j].innerHTML)
-        }
-
-        if (row[1] in categories){
-            categories[row[1]] += parseFloat(row[0])
-        }
-        else{
-            categories[row[1]] = parseFloat(row[0])
-        }
-        total += parseFloat(row[0])
-    }
-
-    for (item in categories){
-        let amount = parseFloat(categories[item])
-        let percentage = amount / total
-
-        updateCategoryTotal(item, amount.toFixed(2), percentage.toFixed(2) * 100)
-    }
-
+    new_row.insertCell(2)
+    updateTotalPercentages(is_expense)
 }
 
 function loadTableFromCSV()
@@ -76,19 +74,21 @@ function loadTableFromCSV()
     data = data.substr(header.length)
 
     let entries = data.split("\n")
-    let table = document.getElementById('table')
+    let table = document.getElementById('history-table')
+
     entries.forEach((value) => {
         let row = table.insertRow(1)
         let values = value.split(",")
 
-        row.style.backgroundColor = parseFloat(values[1]) < 0 ? "red" : "green"
+        let amount = parseFloat(values[1])
+        updateCategoryTotal(values[2], amount.toFixed(2), amount < 0)
+        row.style.backgroundColor = amount < 0 ? "red" : "green"
+
         for (let i = 0; i < 3; ++i){
             let cell = row.insertCell(i)
             cell.innerHTML = values[i]
         }
     })
-
-    updateTotalsTable()
 }
 
 loadTableFromCSV()
@@ -97,7 +97,7 @@ function saveTableAsCSV()
 {
     var csv = []
 
-    let table = document.getElementById("table")
+    let table = document.getElementById("history-table")
     var rows = table.querySelectorAll("tr")
 
     for (var i = 0; i < rows.length; ++i){
@@ -126,7 +126,7 @@ income_button.addEventListener('click', () =>{
 })
 
 ipcRenderer.on('addEntry', (event, args) => {
-    let table = document.getElementById('table')
+    let table = document.getElementById('history-table')
     
     let row = table.insertRow(1)
     row.style.backgroundColor = parseFloat(args[1]) < 0 ? "red" : "green"
@@ -139,7 +139,8 @@ ipcRenderer.on('addEntry', (event, args) => {
     amount.innerHTML = args[1]
     category.innerHTML = args[2]
 
+    updateCategoryTotal(args[2], args[1], args[1] < 0)
     saveTableAsCSV()
-    updateTotalsTable()
+
 })
 
