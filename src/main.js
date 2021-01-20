e4f795d971f4937642b260a92a33d60764621462
Nick Swoboda <nickswoboda@gmail.com>
2021-01-19 16:24:30 -0800

    Improve look of settings window

--- src/main.js
+++ src/main.js
@@ -9,9 +9,10 @@ let budget_win = null
 
 settings.init()
 
-function createBrowser(width, height, has_frame, is_modal)
+function createBrowser(title, width, height, has_frame, is_modal)
 {
     return new BrowserWindow({
+        title: title,
         width: width,
         height: height,
         frame: has_frame,
@@ -20,13 +21,17 @@ function createBrowser(width, height, has_frame, is_modal)
         webPreferences:{
             nodeIntegration: true,
             enableRemoteModule: true
-        }
+        },
+        //only show X button to close if popup
+        minimizable: !is_modal,
+        maximizable: !is_modal,
+        fullscreenable: !is_modal,
     })
 }
 
 function createMainWindow(){
     
-    main_win = createBrowser(1600, 900, true, false)
+    main_win = createBrowser('Budget Manager', 1600, 900, true, false)
     main_win.loadFile('src/views/index.html')
     main_win.on('close', (event) => { 
         main_win = null
@@ -110,7 +115,7 @@ app.on('activate', () => {
 
 function createEntryWindow(is_expense, entry)
 {
-    entry_win = createBrowser(300, 500, false, true)
+    entry_win = createBrowser('Entry', 300, 500, false, true)
     entry_win.on('close', () => {
         entry_win = null
     })
@@ -125,7 +130,8 @@ function createEntryWindow(is_expense, entry)
 
 function createSettingsWindow()
 {
-    settings_win = createBrowser(300, 500, false, true)
+    settings_win = createBrowser('Settings', 220, 180, true, true)
+    settings_win.removeMenu()
     settings_win.on('close', () => {
         settings_win = null
     })
@@ -138,7 +144,7 @@ function createSettingsWindow()
 }
 function createEditUserWindow()
 {
-    user_win = createBrowser(300, 500, false, true)
+    user_win = createBrowser('Users', 300, 500, false, true)
     user_win.on('close', () => {
         user_win = null
     })
@@ -151,7 +157,7 @@ function createEditUserWindow()
 
 function createBudgetWindow()
 {
-    budget_win = createBrowser(300, 500, false, true)
+    budget_win = createBrowser('Budgets', 300, 500, false, true)
     budget_win.on('close', () => {
         budget_win = null
     })
